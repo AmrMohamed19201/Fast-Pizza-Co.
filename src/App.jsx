5a0655661ba166a925e0bc4661c8250b506ef3ee
amr <amr.mohamed19201@gmail.com>
2026-01-15 21:02:13 +0200

    Fetching Data With React Router Loaders: Pizza Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import AppLayout from "./ui/AppLayout";
+//using this method to implement app routing to also let react router control fetching data by using (render as you fetch approach) instead of (fetch on render approach) using useEffects(cause data loading waterfalls)
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
       {
         path: "/menu",
         element: <Menu />,
+        loader: menuLoader, //connect menu loader function to menu route
       },
       {
         path: "/cart",
--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,20 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
 function Menu() {
-  return <h1>Menu</h1>;
+  //provide menu data to Menu component
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem pizza={pizza} key={pizza.id} />
+      ))}
+    </ul>
+  );
 }
-
+//loader function for fetching menu data from api
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
 export default Menu;
